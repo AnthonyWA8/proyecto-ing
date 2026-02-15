60e00f7a74a814897236f2a9fb75afa100271272
anthony <sebastianagva@gmail.com>
2026-02-15 15:00:44 -0500

    clase ejemplar

---
+++ src/Bibliotecario.java
@@ -0,0 +1,9 @@
+public class Bibliotecario extends Usuario {
+
+    private String turno;
+
+    protected Bibliotecario(String id, String nombre, String email, String turno) {
+        super(id, nombre,email);
+        this.turno = turno;
+    }
+}
---
+++ src/Ejemplar.java
@@ -0,0 +1,18 @@
+public class Ejemplar {
+
+    private String codigo;
+    private Libro libro;
+
+    public Ejemplar(String codigo, Libro libro) {
+        this.codigo = codigo;
+        this.libro = libro;
+    }
+
+    public String darCodigo() {
+        return codigo;
+    }
+
+    public Libro darLibro() {
+        return libro;
+    }
+}
